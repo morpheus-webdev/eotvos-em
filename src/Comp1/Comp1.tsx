71639cf34cf45206add29e4156a9388225a14f74
morpheus-webdev <wvwvw22@gmail.com>
2025-02-19 12:52:12 +0100

    init

--- src/Comp1/Comp1.tsx
+++
@@ -1,5 +0,0 @@
-const Comp1 = () => {
-	return <div>Én vagyok Comp1 komponens</div>;
-};
-
-export default Comp1;
--- src/Comp2/Comp2.tsx
+++
@@ -1,3 +0,0 @@
-export const Comp2 = () => {
-	return <div>Én vagyok a Comp2 komponens</div>;
-};
